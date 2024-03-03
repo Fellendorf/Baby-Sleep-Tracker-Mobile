4b1e920971a6572667dc33256791015ff9bd8cdf
Fellendorf <finn85fellendorf@gmail.com>
2024-03-03 03:51:54 +0300

    Some experiments with date/time modals

--- App.js
+++ App.js
@@ -1,30 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import { useState } from 'react';
-import RNDateTimePicker from '@react-native-community/datetimepicker';
+import RNDateTimePicker, { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
 
 export default function App() {
   const [date, setDate] = useState(new Date());
 
-  const onChange = (event, selectedDate) => {
-    console.log(selectedDate);
-    setDate(selectedDate)
-  }
+  const showDatepicker = () => {
+    DateTimePickerAndroid.open({
+      onChange: (event, selectedDate) => {
+        console.log(selectedDate.toLocaleString())
+        setDate(selectedDate,  showTimepicker);
+      },
+      value: date,
+      mode: 'date',
+    });
+  };
+
+  const showTimepicker = () => {
+    DateTimePickerAndroid.open({
+      onChange: (event, selectedDate) => {
+        setDate(selectedDate);
+      },
+      value: date,
+      mode: 'time',
+      display:'spinner',
+      is24Hour: true,
+    });
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <TextInput
-        style={styles.input}
-      />
-      <RNDateTimePicker
+      <Text>Open up App.js to start working on your app!!</Text>
+      {/* <Button
+        title="Add checkpoint"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      /> */}
+      <Button onPress={showDatepicker} title="Add checkpoint" />
+      <Text>selected: {date.toLocaleString()}</Text>
+      {/* <RNDateTimePicker
           display='spinner'
           value={date}
           mode='time'
           is24Hour={true }
           onChange={onChange}
-        />
+        /> */}
     </View>
   );
 }
